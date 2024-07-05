cf38b1aa2ad306a6d536b2bcce4cbf4d8bfcc845
ReaperMz <80462619+ReaperMz@users.noreply.github.com>
2024-07-05 11:02:28 +0530

    Clean Code

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+// Navigation Menu Height
+const navigationHeight = document.querySelector('header.sticky-navbar').clientHeight;
+console.log(navigationHeight)
+document.documentElement.style.setProperty('--nav-height', navigationHeight + "px");
+
 //Persistent Navigation Bar
-const stickyNavbar = document.querySelector("header.sticky-navbar");
+const stickyNavbar = document.querySelector('header.sticky-navbar');
 
 document.addEventListener("scroll", ()=>{
-    if(window.scrollY > 100){
+    if(window.scrollY > 30){
         stickyNavbar.classList.add("scrolling");
     }
     else{
@@ -10,44 +15,71 @@ document.addEventListener("scroll", ()=>{
     }
 })
 
+// Go-to Top Button
+let mybutton = document.getElementById("go-top");
 
-// Navigation Menu Scroll Padding
-const navigationHeight = document.querySelector('.primary-navigation').clientHeight;
-console.log(navigationHeight)
-document.documentElement.style.setProperty('--scroll-padding', navigationHeight + 10 + "px");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
 
-document.addEventListener('DOMContentLoaded', function() {
-    const hamburger = document.querySelector('.hamburger-menu');
-    const navMenu = document.querySelector('.nav-menu');
+function scrollFunction() {
+  if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+    mybutton.style.display = "flex";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
 
-    hamburger.addEventListener('click', function() {
-        hamburger.classList.toggle('active');
-        navMenu.classList.toggle('active');
-    });
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
-    // Close menu when a nav item is clicked
-    document.querySelectorAll('.nav-items').forEach(n => n.addEventListener('click', () => {
-        hamburger.classList.remove('active');
-        navMenu.classList.remove('active');
-    }));
-});
+// ---------------- Carousel Effect ----------------//
 
-//OnScroll Active Navigation Links
-// const sections = document.querySelectorAll('section');
-// const navLinks = document.querySelectorAll('.nav-items');
+// const carousel = document.getElementById('carousel');
+// const prevBtn = document.getElementById('prevBtn');
+// const nextBtn = document.getElementById('nextBtn');
+// const indicators = document.querySelectorAll('.indicator');
+// let index = 0;
+// const slides = document.querySelectorAll('.carousel-slide');
+// const totalSlides = slides.length;
 
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top = window.scrollY;
-//         let offset = sec.offsetTop - 150
-//         let height = sec.offsetHeight;
-//         let id = sec.getAttribute('id');
+// const updateIndicators = () => {
+//     indicators.forEach((indicator, i) => {
+//         indicator.classList.toggle('active', i === index);
+//     });
+// };
 
-//         if(top >= offset && top < offset + height){
-//             navLinks.forEach(link => {
-//                 link.classList.remove('active');
-//             });
-//             document.querySelector('.nav-items').classList.add('active');
-//         }
-//     })
-// }
+// const moveToSlide = (slideIndex) => {
+//     carousel.style.transform = `translateX(${-100 * slideIndex}%)`;
+//     index = slideIndex;
+//     updateIndicators();
+// };
+
+// const moveToNextSlide = () => {
+//     if (index < totalSlides - 1) {
+//         moveToSlide(index + 1);
+//     } else {
+//         moveToSlide(0);
+//     }
+// };
+
+// const moveToPrevSlide = () => {
+//     if (index > 0) {
+//         moveToSlide(index - 1);
+//     } else {
+//         moveToSlide(totalSlides - 1);
+//     }
+// };
+
+// nextBtn.addEventListener('click', moveToNextSlide);
+// prevBtn.addEventListener('click', moveToPrevSlide);
+
+// indicators.forEach((indicator, i) => {
+//     indicator.addEventListener('click', () => {
+//         moveToSlide(i);
+//     });
+// });
+
+// setInterval(moveToNextSlide, 3000);
